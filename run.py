4106ff15a67e06a418c6f6225612cac2c1d12258
yaniwang <yaniwang@redhat.com>
2018-01-09 10:34:06 +0800

    update run.py with args lib

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python2.7
 
-import sys
 import logging
+import argparse
 from utils.helpers import results_logs
 from utils.helpers import generate_final_results
 from utils.helpers import yaml2dict
@@ -10,67 +10,66 @@ from importlib import import_module
 log = logging.getLogger("sherry")
 
 
+def run(tier):
+    """"""
+    config_dict = yaml2dict('./config.yml')
+    host_string = config_dict['host_string']
+    host_user = config_dict['host_user']
+    host_pass = config_dict['host_pass']
+    browser = config_dict['browser']
+    test_build = config_dict['test_build']
+    results_logs.test_build = test_build
+
+    test_scens = yaml2dict('./scen.yml')
+    try:
+        case_files = test_scens[tier]['cases']
+        for cf in case_files:
+            # Get file name from scenario file
+            cf_name = cf.rstrip('.py')
+            results_logs.logger_name = 'checkpoints.log'
+            results_logs.get_actual_logger(cf_name)
+
+            # Import module by case file name
+            case_module = import_module('cases.checks.' + cf_name, __package__)
+            testclss_name = ''
+            for attr in dir(case_module):
+                if attr.startswith('Test'):
+                    testclss_name = attr
+                    break
+            testclss = getattr(case_module, testclss_name, None)
+
+            # Make the instance of test class
+            test = testclss()
+            test.host_string = host_string
+            test.host_user = host_user
+            test.host_pass = host_pass
+            test.browser = browser
+            test.build = test_build
+
+            # Go check
+            log.info(test.go_check(cf_name))
+    except Exception as e:
+        log.exception(e)
+
+    generate_final_results(results_logs)
+
+
 def main():
-    if sys.argv[1] == 'h':
-        str = """Main function entry.
-    Usage:
-        python run.py [argv]
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t",
+        "--test_tier",
+        type=str,
+        choices=[
+            "debug_tier", "he_tier", "virt_tier", "common_tier", "all_tier"
+        ],
+        help="select desired test tier")
 
-    Options:
-        h,help                                          Menu
-        debug_tier                                     Test debug_tier
-        he_tier                                        Test he_tier
-        virt_tier                                      Test virt_tier
-        common_tier                                    Test common_tier
-        all_tier                                       Test all_tier
-    Example:
-        python run.py debug_tier
-    """
-        print str
+    args = parser.parse_args()
+    if args.test_tier is None:
+        parser.print_help()
     else:
-        tier = sys.argv[1]
-
-        config_dict = yaml2dict('./config.yml')
-        host_string = config_dict['host_string']
-        host_user = config_dict['host_user']
-        host_pass = config_dict['host_pass']
-        browser = config_dict['browser']
-        test_build = config_dict['test_build']
-        results_logs.test_build = test_build
-
-        test_scens = yaml2dict('./scen.yml')
-        try:
-            case_files = test_scens[tier]['cases']
-            for cf in case_files:
-                # Get file name from scenario file
-                cf_name = cf.rstrip('.py')
-                results_logs.logger_name = 'checkpoints.log'
-                results_logs.get_actual_logger(cf_name)
-
-                # Import module by case file name
-                case_module = import_module('cases.checks.' + cf_name,
-                                            __package__)
-                testclss_name = ''
-                for attr in dir(case_module):
-                    if attr.startswith('Test'):
-                        testclss_name = attr
-                        break
-                testclss = getattr(case_module, testclss_name, None)
-
-                # Make the instance of test class
-                test = testclss()
-                test.host_string = host_string
-                test.host_user = host_user
-                test.host_pass = host_pass
-                test.browser = browser
-                test.build = test_build
-
-                # Go check
-                log.info(test.go_check(cf_name))
-        except Exception as e:
-            log.exception(e)
-
-        generate_final_results(results_logs)
+        run(args.test_tier)
 
 
 if __name__ == '__main__':
