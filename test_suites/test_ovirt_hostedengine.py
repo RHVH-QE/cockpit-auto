2b589bc8689b5746a1afea2afc156b9424ac68ae
Yihui Zhao <yzhao@redhat.com>
2018-04-19 19:41:12 +0800

    Modify the xpath and other struct

--- test_suites/test_ovirt_hostedengine.py
+++ test_suites/test_ovirt_hostedengine.py
@@ -1,5 +1,4 @@
 import os
-import time
 import sys
 import yaml
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
@@ -18,12 +17,13 @@ class TestOvirtHostedEngine(OvirtHostedEnginePage):
     def test_node_zero_deploy(self):
         a = self.get_data('ovirt_hostedengine.yml')
         config_dict = yaml.load(open(a))
-        self.move_failed_setup_log()
-        self.install_rhvm_appliance(config_dict['rhvm_appliance_path'])
-        self.clean_nfs_storage(
-            config_dict['nfs_ip'],
-            config_dict['nfs_pass'],
-            config_dict['nfs_dir'])
+        def prepare_env():
+            self.move_failed_setup_log()
+            self.install_rhvm_appliance(config_dict['rhvm_appliance_path'])
+            self.clean_nfs_storage(
+                config_dict['nfs_ip'],
+                config_dict['nfs_pass'],
+                config_dict['nfs_dir'])
 
         def check_deploy():
             # VM STAGE
@@ -31,29 +31,32 @@ class TestOvirtHostedEngine(OvirtHostedEnginePage):
             self.browser.input_text(self.VM_FQDN, config_dict['he_vm_fqdn'])
             self.browser.input_text(self.MAC_ADDRESS, config_dict['he_vm_mac'])
             self.browser.input_text(self.ROOT_PASS, config_dict['he_vm_pass'])
-            self.browser.input_text(self.CONFIRM_ROOT_PASS, config_dict['he_vm_pass'])
             self.browser.click(self.NEXT_BUTTON)
 
             # ENGINE STAGE
             self.browser.input_text(self.ADMIN_PASS, config_dict['admin_pass'])
-            self.browser.input_text(self.CONFIRM_ADMIN_PASS, config_dict['admin_pass'])
             self.browser.click(self.NEXT_BUTTON)
 
             # PREPARE VM
             self.browser.click(self.PREPARE_VM_BUTTON)
-            self.browser.click(self.NEXT_BUTTON, 2000)
-            if self.browser.assert_element_visible(self.REDEPLOY_BUTTON):
-                raise Exception("HostedEngine deployment failed on local VM, please check the logs.")
+            # if self.browser.assert_element_visible(self.FAILED_TEXT, 300):
+            #     raise Exception("HostedEngine deployment failed on local VM, please check the logs.")
+            # else:
+            #     pass
+            self.browser.click(self.NEXT_BUTTON, 600)
 
             # STORAGE STAGE
             self.browser.input_text(self.STORAGE_CONN, config_dict['nfs_ip'] + ':' + config_dict['nfs_dir'])
             self.browser.click(self.NEXT_BUTTON)
 
             #FINISH STAGE
-            self.browser.click(self.FINISH_DEVELOPMENT)
-            self.browser.click(self.CLOSE_BUTTON, 2000)
-            if self.browser.assert_element_visible(self.REDEPLOY_BUTTON):
-                raise Exception("HostedEngine deployment failed on target VM, please check the logs")
+            # self.browser.click(self.FINISH_DEVELOPMENT)
+            # if self.browser.assert_element_visible(self.FAILED_TEXT, 500):
+            #     raise Exception("HostedEngine deployment failed on target VM, please check the logs")
+            # else:
+            #     pass
+            self.browser.click(self.CLOSE_BUTTON, 1500)
+        prepare_env()
         check_deploy()
 
     def test_hostedengine_deployed(self):
@@ -87,25 +90,25 @@ class TestOvirtHostedEngine(OvirtHostedEnginePage):
             config_dict['admin_pass'])
 
     def test_local_maintenance(self):
-        local_mainten_stat = "XPATH{}//div[@class='list-group-item-text']"
-        # self.browser.assert_element_visible(local_mainten)
         self.put_host_to_local_maintenance()
-        time.sleep(120)
-        self.browser.assert_text_expected(local_mainten_stat, 'true')
+        self.browser.assert_text_expected(self.LOCAL_MAINTEN_STAT, 'true')
 
     def test_migrated_he(self):
-        vm_status = "XPATH{}//div[contains(@class, 'list-view-pf-additional-info-item')]/div"
-        self.browser.assert_text_expected(vm_status, 'down')
+        self.browser.assert_text_expected(self.VM_STATUS, 'down')
 
     def test_remove_maintenance(self):
-        local_mainten_stat = "XPATH{}//div[@class='list-group-item-text']"
         self.remove_host_from_maintenance()
-        time.sleep(30)
-        self.browser.assert_text_not_expected(local_mainten_stat, 'true')
+        self.browser.assert_text_not_expected(self.LOCAL_MAINTEN_STAT, 'true')
 
     def test_global_maintenance(self):
         self.put_cluster_to_global_maintenance()
         self.browser.assert_element_visible(self.GLOBAL_HINT)
 
     def test_hostedengine_redeploy(self):
-        pass
+        """
+        :avocado: tags=he
+        """
+        self.clean_hostengine_env()
+        self.test_node_zero_deploy()
+
+        
