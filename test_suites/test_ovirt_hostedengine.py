c25df449f8bc90838f0cad10f13de10894b3790c
Yihui Zhao <yzhao@redhat.com>
2018-05-15 17:07:39 +0800

    encapsulate element operation in HE page and improve the HE test cases

--- test_suites/test_ovirt_hostedengine.py
+++ test_suites/test_ovirt_hostedengine.py
@@ -12,86 +12,38 @@ class TestOvirtHostedEngine(OvirtHostedEnginePage):
     """
 
     def test_guide_link(self):
-        self.assert_element_visible(self.GETTING_START_LINK)
-        self.assert_element_visible(self.MORE_INFORMATION_LINK)
-
-    def default_VM_ENGINE_CONFIG(self):
-        a = self.get_data('ovirt_hostedengine.yml')
-        config_dict = yaml.load(open(a))
-
-        # VM STAGE
-        self.click(self.HE_START)
-        self.input_text(self.VM_FQDN, config_dict['he_vm_fqdn'])
-        self.input_text(self.MAC_ADDRESS, config_dict['he_vm_mac'])
-        self.input_text(self.ROOT_PASS, config_dict['he_vm_pass'])
-        self.click(self.NEXT_BUTTON)
-
-        # ENGINE STAGE
-        self.input_text(self.ADMIN_PASS, config_dict['admin_pass'])
-        self.click(self.NEXT_BUTTON)
-
-        # PREPARE VM
-        self.click(self.PREPARE_VM_BUTTON)
-        self.click(self.NEXT_BUTTON, 1500)
+        self.check_guide_link()
 
     def test_node_zero_default_deploy(self):
         # The default deployment means that HE deployment, DHCP network, NFS Auto version, No MNT Option
         """
         :avocado: tags=he_tier1
         """
-        a = self.get_data('ovirt_hostedengine.yml')
-        config_dict = yaml.load(open(a))
-
-        def prepare_env():
-            self.move_failed_setup_log()
-            self.install_rhvm_appliance(config_dict['rhvm_appliance_path'])
-            self.clean_nfs_storage(config_dict['nfs_ip'],
-                                   config_dict['nfs_pass'],
-                                   config_dict['nfs_dir'])
-
-        def check_deploy():
-            self.default_VM_ENGINE_CONFIG()
-
-            # STORAGE STAGE
-            self.input_text(
-                self.STORAGE_CONN,
-                config_dict['nfs_ip'] + ':' + config_dict['nfs_dir'])
-            self.click(self.NEXT_BUTTON)
-
-            # FINISH STAGE
-            self.click(self.FINISH_DEPLOYMENT)
-            self.click(self.CLOSE_BUTTON, 1500)
-
-        prepare_env()
-        check_deploy()
+        self.node_zero_default_deploy_process()
 
     def test_hostedengine_deployed(self):
         """
         :avocado: tags=he_tier1
         """
-        self.assert_element_visible(self.HE_RUNNING)
+        self.check_hostedengine_deployed()
 
     def test_maintenance_hint(self):
         """
         :avocado: tags=he_tier1
         """
-        self.assert_element_visible(self.MAINTENANCE_HINT)
+        self.check_maintenance_hint()
 
     def test_engine_vm_status(self):
         """
         :avocado: tags=he_tier1
         """
-        self.assert_element_visible(self.ENGINE_UP_ICON)
-        self.assert_element_visible(self.HE_RUNNING)
+        self.check_engine_vm_status()
 
     def test_no_password_saved(self):
         """
         :avocado: tags=he_tier1
         """
-        a = self.get_data('ovirt_hostedengine.yml')
-        config_dict = yaml.load(open(a))
-        self.check_no_password_saved(config_dict['he_vm_pass'],
-                                     config_dict['admin_pass'])
+        self.check_no_password_saved_in_setup_log()
 
     def test_no_large_messages(self):
         """
@@ -103,192 +55,58 @@ class TestOvirtHostedEngine(OvirtHostedEnginePage):
         """
         :avocado: tags=he_tier2
         """
-        a = self.get_data('ovirt_hostedengine.yml')
-        config_dict = yaml.load(open(a))
-
-        def prepare_env():
-            self.move_failed_setup_log()
-            self.install_rhvm_appliance(config_dict['rhvm_appliance_path'])
-
-        def check_deploy():
-            self.default_VM_ENGINE_CONFIG()
-
-            # STORAGE STAGE
-            self.click(self.STORAGE_BUTTON)
-            self.click(self.STORAGE_ISCSI)
-            self.click(self.STORAGE_ADVANCED)
-            self.input_text(self.PORTAL_IP_ADDR, config_dict['iscsi_portal_ip'])
-            self.input_text(self.PORTAL_USER, config_dict['iscsi_portal_user'])
-            self.input_text(self.PORTAL_PASS, config_dict['iscsi_portal_pass'])
-            self.input_text(self.DISCOVERY_USER, config_dict['iscsi_discovery_user'])
-            self.input_text(self.DISCOVERY_PASS, config_dict['iscsi_discovery_pass'])
-
-            self.click(self.RETRIEVE_TARGET)
-            self.click(self.SELECTED_TARGET, 60)
-            self.click(self.SELECTED_ISCSI_LUN, 60)
-            self.click(self.NEXT_BUTTON)
-
-            # FINISH STAGE
-            self.click(self.FINISH_DEPLOYMENT)
-            self.click(self.CLOSE_BUTTON, 1500)
-
-        prepare_env()
-        check_deploy()
+        self.node_zero_iscsi_deploy_process()
 
     def test_node_zero_fc_deployment(self):
         """
         :avocado: tags=he_tier2
         """
-        a = self.get_data('ovirt_hostedengine.yml')
-        config_dict = yaml.load(open(a))
-
-        def prepare_env():
-            self.move_failed_setup_log()
-            self.install_rhvm_appliance(config_dict['rhvm_appliance_path'])
-
-        def check_deploy():
-            self.default_VM_ENGINE_CONFIG()
-
-            # STORAGE STAGE
-            self.click(self.STORAGE_BUTTON)
-            self.click(self.STORAGE_FC)
-
-            self.click(self.SELECTED_FC_LUN, 60)
-
-            self.click(self.NEXT_BUTTON)
-
-            # FINISH STAGE
-            self.click(self.FINISH_DEPLOYMENT)
-            self.click(self.CLOSE_BUTTON, 1500)
-
-        prepare_env()
-        check_deploy()
+        self.node_zero_fc_deploy_process()
 
     def test_node_zero_gluster_deployment(self):
         """
         :avocado: tags=he_tier2
         """
-        a = self.get_data('ovirt_hostedengine.yml')
-        config_dict = yaml.load(open(a))
-
-        def prepare_env():
-            self.move_failed_setup_log()
-            self.install_rhvm_appliance(config_dict['rhvm_appliance_path'])
-
-        def check_deploy():
-            self.default_VM_ENGINE_CONFIG()
-
-            # STORAGE STAGE
-            self.click(self.STORAGE_BUTTON)
-            self.click(self.STORAGE_GLUSTERFS)
-            self.input_text(
-                self.STORAGE_CONN,
-                config_dict['gluster_ip'] + ':' + config_dict['gluster_dir'])
-            self.click(self.NEXT_BUTTON)
-
-            # FINISH STAGE
-            self.click(self.FINISH_DEPLOYMENT)
-            self.click(self.CLOSE_BUTTON, 1500)
-
-        prepare_env()
-        check_deploy()
+        self.node_zero_gluster_deploy_process()
 
     def test_node_zero_static_v4_deploy(self):
         """
         :avocado: tags=he_tier2
         """
-        a = self.get_data('ovirt_hostedengine.yml')
-        config_dict = yaml.load(open(a))
-
-        def prepare_env():
-            self.move_failed_setup_log()
-            self.install_rhvm_appliance(config_dict['rhvm_appliance_path'])
-            self.clean_nfs_storage(config_dict['nfs_ip'],
-                                   config_dict['nfs_pass'],
-                                   config_dict['nfs_dir'])
-
-        def check_deploy():
-            # VM STAGE
-            self.click(self.HE_START)
-            self.input_text(self.VM_FQDN, config_dict['he_vm_fqdn'])
-            self.input_text(self.MAC_ADDRESS, config_dict['he_vm_mac'])
-            self.click(self.NETWORK_DROPDOWN)
-            self.click(self.NETWORK_STATIC)
-            self.input_text(self.VM_IP, config_dict['he_vm_ip'])
-            self.input_text(self.IP_PREFIX, config_dict['he_ip_prefix'])
-            self.input_text(self.DNS_SERVER, config_dict['dns_server'])
-            self.input_text(self.ROOT_PASS, config_dict['he_vm_pass'])
-            self.click(self.NEXT_BUTTON)
-
-            # ENGINE STAGE
-            self.input_text(self.ADMIN_PASS, config_dict['admin_pass'])
-            self.click(self.NEXT_BUTTON)
-
-            # PREPARE VM
-            self.click(self.PREPARE_VM_BUTTON)
-            self.click(self.NEXT_BUTTON, 1500)
-
-            # STORAGE STAGE
-            self.input_text(
-                self.STORAGE_CONN,
-                config_dict['nfs_ip'] + ':' + config_dict['nfs_dir'])
-            self.click(self.STORAGE_ADVANCED)
-            self.click(self.NFS_VER_DROPDOWN)
-            self.click(self.NFS_V4)
-            self.click(self.NEXT_BUTTON)
-
-            # FINISH STAGE
-            self.click(self.FINISH_DEPLOYMENT)
-            self.click(self.CLOSE_BUTTON, 1500)
-
-        prepare_env()
-        check_deploy()
+        self.node_zero_gluster_deploy_process()
 
     def test_hostedengine_redeploy(self):
         """
         :avocado: tags=he_tier2
         """
-        self.clean_hostengine_env()
-        self.refresh()
-        self.switch_to_frame(self.OVIRT_HOSTEDENGINE_FRAME_NAME)
-        self.test_node_zero_default_deploy()
+        self.hostedengine_redeploy_process()
 
     def test_additional_host(self):
         """
         :avocado: tags=he_tier1
         """
-        a = self.get_data('ovirt_hostedengine.yml')
-        config_dict = yaml.load(open(a))
-        self.add_additional_host_to_cluster(
-            config_dict['second_host'], config_dict['second_vm_fqdn'],
-            config_dict['second_pass'], config_dict['he_vm_fqdn'],
-            config_dict['admin_pass'])
-        self.check_additional_host_socre(config_dict['second_host'],
-                                         config_dict['second_pass'])
+        self.add_additional_host_to_cluster_process()
 
     def test_local_maintenance(self):
         """
         :avocado: tags=he_tier1
         """
-        self.put_host_to_local_maintenance()
-        self.assert_text_in_element(self.LOCAL_MAINTEN_STAT, 'true')
+        self.check_local_maintenance()
 
     def test_migrated_he(self):
         """
         :avocado: tags=he_tier1
         """
-        self.assert_text_in_element(self.VM_STATUS, 'down')
+        self.check_migrated_he()
 
     def test_remove_maintenance(self):
         """
         :avocado: tags=he_tier1
         """
-        self.remove_host_from_maintenance()
-        self.assert_text_not_in_element(self.LOCAL_MAINTEN_STAT, 'true')
+        self.check_remove_maintenance()
 
     def test_global_maintenance(self):
         """
         :avocado: tags=he_tier1
         """
-        self.put_cluster_to_global_maintenance()
-        self.assert_element_visible(self.GLOBAL_HINT)
+        self.check_global_maintenance
