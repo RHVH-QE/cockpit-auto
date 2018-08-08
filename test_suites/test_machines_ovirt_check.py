52c3a72ab8814fa1cf6666ea7ed832ffc9b43ac1
kenzhaoyihui <2993614148@qq.com>
2018-08-08 17:13:09 +0800

    machines-ovirt and he: change polarion map by using decorator

--- test_suites/test_machines_ovirt_check.py
+++ test_suites/test_machines_ovirt_check.py
@@ -2,6 +2,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from page_objects.page_machines_ovirt_check import MachinesOvirtCheckPage
+from utils.caseid import add_case_id
 
 
 class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
@@ -9,10 +10,11 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
     :avocado: enable
     :avocado: tags=he_vm
     """
-
+    @add_case_id("RHEL-118144")
     def test_disable_create_new_vm(self):
         self.disable_create_new_vm()
 
+    @add_case_id("RHEL-113784")
     def test_overview_info(self):
         self.get_dumpxml_on_host()
         self.open_vm_row()
@@ -21,12 +23,14 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
             value_in_ui = self.get_overview_info_on_ui(key)
             self.assertEqual(value_in_xml, value_in_ui)
 
+    @add_case_id("RHEL-113785")
     def test_cpu_memory_usage_info(self):
         self.open_vm_row()
         self.open_usage_subtab()
         self.assertNotEqual(self.get_cpu_usage_on_ui(), "0.0%")
         self.assertNotEqual(self.get_memory_usage_on_ui(), "0.00GiB")
 
+    @add_case_id("RHEL-113787")
     def test_disks_info(self):
         self.get_dumpxml_on_host()
         self.open_vm_row()
@@ -41,6 +45,7 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
                 value_in_ui = self.get_disk_info_on_ui(target, column)
                 self.assertEqual(value_in_xml, value_in_ui)
 
+    @add_case_id("RHEL-118139")
     def test_networks_info(self):
         self.get_dumpxml_on_host()
         self.open_vm_row()
@@ -53,6 +58,7 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
                 value_on_ui = self.get_network_info_on_ui(i + 1, column)
                 self.assertEqual(value_in_xml, value_on_ui)
 
+    @add_case_id("RHEL-113788")
     def test_inline_vnc_console(self):
         self.open_vm_row()
         self.open_consoles_subtub()
@@ -61,6 +67,7 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
         else:
             self.assertEqual(self.get_console_type(), self.EXTERNAL_CONSOLE_NAME)
 
+    @add_case_id("RHEL-113789")
     def test_ctrl_alt_del(self):
         self.open_vm_row()
         self.open_consoles_subtub()
@@ -69,6 +76,8 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
         else:
             self.assertEqual(self.get_console_type(), self.EXTERNAL_CONSOLE_NAME)
 
+    @add_case_id("RHEL-118141")
+    @add_case_id("RHEL-113793")
     def test_external_console(self):
         self.open_vm_row()
         self.open_consoles_subtub()
@@ -84,12 +93,14 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
         for viewer in viewer_list:
             self.assertEqual(self.get_consoles_manual_port_on_ui(viewer['type']), viewer['port'])
 
+    @add_case_id("RHEL-113792")
     def test_external_console_more_info(self):
         self.open_vm_row()
         self.open_consoles_subtub()
         self.open_external_console_page()
         self.toggle_more_info()
 
+    @add_case_id("RHEL-113794")
     def test_ovirt_info(self):
         self.open_vm_row()
         self.open_ovirt_subtub()
@@ -98,6 +109,7 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
             value_in_ui = self.get_ovirt_info_on_ui(key)
             self.assertEqual(value_in_host, value_in_ui)
 
+    @add_case_id("RHEL-139377")
     def test_heVm_network_change(self):
         self.open_vm_row()
         self.open_networks_subtab()
@@ -107,7 +119,7 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
         self.assertNotEqual(self.get_network1_state_on_ui(), 'down')
         self.assertNotEqual(self.get_network1_state_on_host(), 'down')
 
-
+    @add_case_id("RHEL-118140")
     def test_ovirtVm_network_unplug(self):
         self.open_vm_row()
         self.open_networks_subtab()
@@ -117,6 +129,7 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
         self.assertEqual(self.get_network1_state_on_ui(), 'down')
         self.assertEqual(self.get_network1_state_on_host(), 'down')
 
+    @add_case_id("RHEL-139378")
     def test_ovirtVm_network_plug(self):
         self.open_vm_row()
         self.open_networks_subtab()
@@ -126,51 +139,63 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
         self.assertEqual(self.get_network1_state_on_ui(), 'up')
         self.assertEqual(self.get_network1_state_on_host(), 'up')
 
+    @add_case_id("RHEL-138481")
     def test_restart_heVm(self):
         self.open_vm_row()
         self.reboot_he_vm_on_ui()
 
+    @add_case_id("RHEL-138482")
     def test_force_reboot_heVm(self):
         self.open_vm_row()
         self.force_reboot_he_vm_on_ui()
 
+    @add_case_id("RHEL-138483")
     def test_shutdown_heVm(self):
         self.open_vm_row()
         self.shutdown_he_vm_on_ui()
 
+    @add_case_id("RHEL-138484")
     def test_forceoff_heVm(self):
         self.open_vm_row()
         self.forceoff_he_vm_on_ui()
 
+    @add_case_id("RHEL-139379")
     def test_sendnmi_heVm(self):
         self.open_vm_row()
         self.sendnmi_he_vm_on_ui()
 
+    @add_case_id("RHEL-139380")
     def test_supend_heVm(self):
         pass
         # self.open_vm_row()
         # self.suspend_he_vm_on_ui()
 
+    @add_case_id("RHEL-113796")
     def test_restart_ovirtVm(self):
         self.open_vm_row()
         self.reboot_ovirt_vm_on_ui()
 
+    @add_case_id("RHEL-113797")
     def test_force_reboot_ovirtVm(self):
         self.open_vm_row()
         self.force_reboot_ovirt_vm_on_ui()
 
+    @add_case_id("RHEL-113798")
     def test_shutdown_ovirtVm(self):
         self.open_vm_row()
         self.shutdown_ovirt_vm_on_ui()
 
+    @add_case_id("RHEL-113799")
     def test_forceoff_ovirtVm(self):
         self.open_vm_row()
         self.forceoff_ovirt_vm_on_ui()
 
+    @add_case_id("RHEL-113800")
     def test_sendnmi_ovirtVm(self):
         self.open_vm_row()
         self.sendnmi_ovirt_vm_on_ui()
 
+    @add_case_id("RHEL-113820")
     def test_supend_ovirtVm(self):
         self.open_vm_row()
         self.suspend_ovirt_vm_on_ui()
@@ -178,15 +203,19 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
     def test_vdsm_conf_in_ui(self):
         self.check_vdsm_conf_in_ui()
 
+    @add_case_id("RHEL-113776")
     def test_save_vdsm_conf_in_ui(self):
         self.check_save_vdsm_conf_in_ui()
 
+    @add_case_id("RHEL-113777")
     def test_vdsm_service_link(self):
         self.click_vdsm_service_mgmt()
 
+    @add_case_id("RHEL-113778")
     def test_reload_vdsm_conf(self):
         self.check_reload_vdsm_conf_in_ui()
 
+    @add_case_id("RHEL-113781")
     def test_cluster_info(self):
         self.get_dumpxml_on_host()
         self.click(self.CLUSTER_TOPNAV)
@@ -195,14 +224,17 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
             value_in_ui = self.get_cluster_info_in_ui(key)
             self.assertEqual(value_in_host, value_in_ui)
 
+    @add_case_id("RHEL-113782")
     def test_cluster_host_link(self):
         self.click(self.CLUSTER_TOPNAV)
         self.click_cluster_host_link()
 
+    @add_case_id("RHEL-114121")
     def test_run_vm_in_cluster(self):
         self.open_vm_row()
         self.run_vm_in_cluster()
 
+    @add_case_id("RHEL-118147")
     def test_template_info(self):
         self.click(self.TEMPLATES_TOPNAV)
         for key in self.TEMPLATE_INFO_NAME:
@@ -210,27 +242,33 @@ class TestMachinesOvirtCheck(MachinesOvirtCheckPage):
             value_in_ui = self.get_template_info_in_ui(key)
             self.assertEqual(value_in_host, value_in_ui)
 
+    @add_case_id("RHEL-113779")
     def test_create_vm_by_template(self):
         self.click(self.TEMPLATES_TOPNAV)
         self.create_vm_by_template()
 
+    @add_case_id("RHEL-113780")
     def test_create_vm_twice(self):
         self.click(self.TEMPLATES_TOPNAV)
         self.check_create_vm_twice()
 
+    @add_case_id("RHEL-113795")
     def test_vm_migration(self):
         self.open_vm_row()
         self.open_ovirt_subtub()
         self.migrate_vm_to_additional_host()
 
+    @add_case_id("RHEL-114114")
     def test_host_to_maintenance(self):
         self.host_to_maintenance()
 
+    @add_case_id("RHEL-113786")
     def test_vm_icon(self):
         self.open_vm_row()
         self.assertEqual (self.get_vm_icon_data_on_host(), 
                 self.get_vm_icon_data_on_ui())
 
+    @add_case_id("RHEL-113974")
     def test_non_root_user(self):
         self.login_non_root_user()
         self.assert_element_invisible(self.VM_ROW)
--- test_suites/test_machines_ovirt_package.py
+++ test_suites/test_machines_ovirt_package.py
@@ -4,6 +4,7 @@ import yaml
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from avocado import Test
 from utils.machine import Machine
+from utils.caseid import add_case_id 
 
 BASE_URL = "{base}/{ver1}/{ver2}/{arch}/{name}"
 
@@ -38,6 +39,7 @@ class TestMachinesOvirtPackage(Test):
             cmd = 'curl -o {} {}'.format(rpm_name, url)
             self.host.execute(cmd)
 
+    @add_case_id("RHEL-113962")
     def test_upgrade_pkg(self):
         cmd = 'rpm -e cockpit-machines-ovirt --nodeps'
         self.host.execute(cmd)
@@ -49,6 +51,7 @@ class TestMachinesOvirtPackage(Test):
         ret = self.host.execute(cmd, raise_exception=False)
         self.assertEqual(ret + '.rpm', self.NEW_MACHINES_RPM_NAME)
 
+    @add_case_id("RHEL-113961")
     def test_remove_pkg(self):
         cmd = 'rpm -e cockpit-machines-ovirt --nodeps'
         self.host.execute(cmd)
@@ -56,6 +59,7 @@ class TestMachinesOvirtPackage(Test):
         ret = self.host.execute(cmd, raise_exception=False)
         self.assertEqual(ret, '')
 
+    @add_case_id("RHEL-113768")
     def test_install_pkg(self):
         cmd = 'rpm -ivh {}'.format(self.NEW_MACHINES_RPM_NAME)
         self.host.execute(cmd)
